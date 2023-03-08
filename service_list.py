f38b8b09f6d562bb2bc4890d282fb104fb66a33f
vijay2181 <66196388+vijay2181@users.noreply.github.com>
2023-03-08 14:21:59 +0530

    Create service_list.py

---
+++ service_list.py
@@ -0,0 +1,18 @@
+import time
+#10/10   is runningCount/DesiredCount
+
+time.sleep(1)
+print("service1:1/1")
+time.sleep(1)
+print("service2:10/10")
+time.sleep(1)
+print("service3:1/1")
+time.sleep(1)
+print("service4:1/1")
+time.sleep(1)
+print("service5:2/2")
+time.sleep(1)
+print("service6:1/1")
+time.sleep(1)
+print("service7:5/5")
+time.sleep(1)
